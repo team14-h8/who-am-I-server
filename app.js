efae9e3949cfaa4ab355d74d071954656ed6cacd
ical10 <mochhusnir@gmail.com>
2021-01-22 11:14:07 +0700

    adding emit to get all users and send a message when a new user join

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ const io = require('socket.io')(server)
 const PORT = process.env.PORT || 5002
 
 // a comment to start development branch
-const message = "welcome to Tic-Tac-Toe!"
+const newPlayerMessage = "a new challenger has entered the game!"
+
+const users = []
 
 io.on('connection', function (socket) {
+  ID = socket.id
   // client connect, masuk ke sini
   console.log('Socket.io client connected')
   // server akan membalas dengan ini
-  socket.emit('init', { message })
+  socket.emit('init', users)
 
-  socket.on('newMessage', function (payload) {
-    //socket.broadcast.emit // send to other users expect the client
+
+  socket.on('newPlayer', function (payload) {
+    payload.id = ID
+    users.push(payload)
+    io.emit('getAllUsers', users) // send to other users expect the client
+    io.emit('newPlayer', { message: newPlayerMessage })
     //io.emit // send to everyone including the client
   })
 })
